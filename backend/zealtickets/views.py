2c86683c57281845fbf9412776f60f357c0b9edb
clencyc <clency2023@gmail.com>
2024-11-04 23:30:39 +0300

    django

---
+++ backend/zealtickets/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.http import HttpResponse, JsonResponse
+from django.views.generic import View
+from datetime import datetime
+from django.shortcuts import render
+
+
+
+def index(request):
+    return render(request, "index.html")
+
